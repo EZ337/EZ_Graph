b5a8561861a0a3eaa00c6b2ba269215f5e775ea8
ez337 <ezgaming3377@gmail.com>
2024-05-06 22:09:56 +0900

    Moved stuff to .h file

--- Graph.cpp
+++ Graph.cpp
@@ -1,49 +1,5 @@
-#include <iostream>
-#include <iterator>
-#include <stdexcept>
-
 #include "Graph.h"
 
-template <typename IDType>
-void Vertex<IDType>::AddIncidentVertex(Vertex<IDType>* other)
-{
-    this->IncidentVertices.insert(other);
-
-    //TODO: Create an edge as well
-}
-
-template <typename IDType>
-Vertex<IDType>* Graph<IDType>::AddVertex(IDType id)
-{
-    // Throw error if id is nullID
-    if (id == NULLVALUE)
-    {
-        throw std::invalid_argument("Vertex ID cannot be the NullID. NullID is " + std::to_string(NULLVALUE));
-    }
-
-    Vertex<IDType> vert(id); // Create new vertex
-    typename std::set<Vertex<IDType>>::iterator v = vertices.insert(vert).first; // Get the inserted vertex
-    
-    return const_cast<Vertex<IDType>*>(&(*v)); // Return pointer to the vertex
-}
-
-template <typename IDType>
-void Graph<IDType>::AddToGraph(IDType v1, IDType v2)
-{
-    Vertex<IDType>* V1 = AddVertex(v1);
-
-    // Add v2 and create an edge if v2 is not null
-    if (v2 != NULLVALUE)
-    {
-        Vertex<IDType>* V2 = AddVertex(v2);
-
-        // Add connection between the vertices
-        // Undirected/BiDirectional Graph
-        V1->AddIncidentVertex(V2);
-        V2->AddIncidentVertex(V1);
-    }
-
-}
 
 int main()
 {
--- Graph.h
+++ Graph.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <set>
 #include <map>
+#include <stdexcept>
 
 template <typename IDType = int>
 struct Vertex
@@ -12,17 +13,20 @@ struct Vertex
 
     IDType id; // ID of the vertex. Integers are cleaner to deal with
     /// Set of pointers to adjacent vertices
-    std::set<Vertex<IDType>*> IncidentVertices;
+    std::set<Vertex<IDType>*> incident_vertices;
     bool visited = false;
 
     /// @brief Creates a vertex with id v_id
     /// @param v_id ID of the created vertex
     Vertex(IDType v_id) : id(v_id) {}
 
-    void AddIncidentVertex(Vertex<IDType>* other);
+    void AddIncidentVertex(Vertex<IDType>* other)
+    {
+        this->incident_vertices.insert(other);
+    }
 
     /// @return The degree of this vertex
-    int degree() {return IncidentVertices.size();}
+    int degree() {return incident_vertices.size();}
 
     /// @brief Compares vertex with other vertex. Note, IDType must
     /// have a valid operator== overload.
@@ -53,7 +57,7 @@ class Graph
 {
 private:
     /// Serves as the NULLID of a verrtex
-    IDType NULLVALUE;
+    IDType null_id;
 
     std::set<Vertex<IDType>> vertices;  
 
@@ -62,15 +66,41 @@ public:
     /// @brief Graph constructor. nullrepr is the value that will serve
     /// as the null representation of a vertex. Used in AddToGraph.
     /// @param nullrepr Vertex with this ID will be ignored
-    Graph(IDType nullrepr) : NULLVALUE(nullrepr) {}
+    Graph(IDType nullrepr) : null_id(nullrepr) {}
 
-    Vertex<IDType>* AddVertex(IDType);
+    Vertex<IDType>* AddVertex(IDType id)
+    {
+        // Throw error if id is nullID
+        if (id == null_id)
+        {
+            throw std::invalid_argument("Vertex ID cannot be the NullID. NullID is " + std::to_string(null_id));
+        }
+
+        Vertex<IDType> vert(id); // Create new vertex
+        typename std::set<Vertex<IDType>>::iterator v = vertices.insert(vert).first; // Get the inserted vertex
+        
+        return const_cast<Vertex<IDType>*>(&(*v)); // Return pointer to the vertex
+    }
     //void AddEdge(IDType v1, IDType v2);
 
     /// @brief Adds vertices v1 and v2 and creates a link betweeen them
     /// @param v1 Vertex 1
     /// @param v2 Vertex 2
-    void AddToGraph(IDType v1, IDType v2);
+    void AddToGraph(IDType v1, IDType v2)
+    {
+        Vertex<IDType>* V1 = AddVertex(v1);
+
+        // Add v2 and create an edge if v2 is not null
+        if (v2 != null_id)
+        {
+            Vertex<IDType>* V2 = AddVertex(v2);
+
+            // Add connection between the vertices
+            // Undirected/BiDirectional Graph
+            V1->AddIncidentVertex(V2);
+            V2->AddIncidentVertex(V1);
+        }
+    }
 
 };
 
